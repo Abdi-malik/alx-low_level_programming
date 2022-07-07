b9bb0e0b5dae305d58a9231c9862c893e89aa401
Abdimalik Odowa <cabdimalikodowa@gmail.com>
2022-07-07 10:53:25 +0000

    checks upper alphabet case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isupper - checks upper case letter
+* @c: it will display 1
+* Return: if the Letter be Alphabet upper 1 if it is not it returns 0
+*/
+
+int _isupper(int c);
+{
+	return(c >= 65 && c <= 90);
+}
