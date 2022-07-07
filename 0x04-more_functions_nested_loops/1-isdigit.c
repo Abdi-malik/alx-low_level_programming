fb7bcb212b0b24529d139bc616fdb054b5d5218b
Abdimalik Odowa <cabdimalikodowa@gmail.com>
2022-07-07 11:13:39 +0000

    tasks of 0 - 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isdigit - checks whether c is digit or not
+* @c: is holding input that is processed _isdigit
+* Return: retuns something that it is value is 1 or 0
+*/
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - multiplies two numbers
+* @a: holds an integer
+* @b: holds an other integer
+* Return: returns result
+*/
+
+int mul(int a, int b)
+{
+	result = a * b;
+	return (result);
+}
